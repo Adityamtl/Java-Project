74a84137a360fe324992a83f4e895f908d967f04
Aditya Mittal <Aditya.Mittal@iiitb.ac.in>
2025-11-22 00:53:39 +0530

    Remove duplicate files - keep only digital-banking-system folder

--- src/main/java/com/spring_project/digital_banking_system/model/TransactionStatus.java
+++
@@ -1,6 +0,0 @@
-package com.spring_project.digital_banking_system.model;
-
-public enum TransactionStatus {
-    SUCCESS,
-    FAILED
-}
--- src/main/java/com/spring_project/digital_banking_system/model/TransactionType.java
+++
@@ -1,8 +0,0 @@
-package com.spring_project.digital_banking_system.model;
-
-public enum TransactionType {
-    DEPOSIT,
-    WITHDRAWAL,
-    TRANSFER,
-    BANK_TRANSFER
-}
